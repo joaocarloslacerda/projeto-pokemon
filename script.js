5a7cb3194b6b47907947944a13218ff4a8171855
joao <juniorlacerdasilva@gmail.com>
2024-04-06 19:58:03 -0300

    Adicionada funcionalidade para aparecer a imagem do pokemon
    Ajustando funcionalidade para mostrar os tipos do pokemon

--- script.js
+++ script.js
@@ -8,9 +8,36 @@ async function buscarPokemon(){
     const pokemon = await resposta.json()
 
     const divNomePersonagem = document.getElementById("nomePersonagem")
-    divNomePersonagem.innerText = pokemon.name
+    divNomePersonagem.innerText = `#${pokemon.order} - ${pokemon.name}`
 
     const divImgPersonagem = document.getElementById("imgPersonagem")
     divImgPersonagem.src = pokemon.sprites.front_default
 
+    adicionaTipos(pokemon)
+
+}
+async function adicionaTipos(pokemon){
+
+    const divTipoUm = document.createElement('div')
+    divTipoUm.classList.add('tipoUm')
+
+    const divTipoDois = document.createElement('div')
+    divTipoUm.classList.add('tipoDois')
+
+    pokemon.types.forEach(element => {
+        console.log(element.type.name)
+        if(element.type.name = 'grass'){
+            divTipoUm.innerText = (element.type.name)
+            document.body.appendChild(divTipoUm)
+            console.log("teste1")
+        }
+        else if(element.type.name = 'poison'){
+            divTipoDois.innerText = (element.type.name)
+            document.body.appendChild(divTipoDois)
+            console.log("teste2")
+        }
+    });
+    console.log(pokemon)
+
+    // console.log(element.type.name)
 }
